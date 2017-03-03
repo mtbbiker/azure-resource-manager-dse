2d3dbe211d9dc2d268023ddad42e7910091862cc
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-02 17:10:25 -0800

    fix url

--- singledc/deploy.bat
+++
@@ -1,2 +0,0 @@
-New-AzureRmResourceGroup -Location "germanynortheast" -Name "TestDataStax-NE"
-New-AzureRmResourceGroupDeployment -Name "TestDataStaxDeployment" -ResourceGroupName "TestDataStax-NE" -TemplateFile ".\mainTemplate.json" -TemplateParameterFile ".\mainTemplateParameters.json" -Verbose
