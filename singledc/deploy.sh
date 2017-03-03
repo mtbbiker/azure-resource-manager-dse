2130c44ebebcd5fbdae6df167b518947e8334ef7
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-03 09:23:00 -0800

    switch to west2 default

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -2,5 +2,5 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "westus"
+azure group create $1 "westus2"
 azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
