7201e2d2f9c27cdf653cf1903a3b6a7fcdff9fca
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-06 10:12:36 -0800

    add tcp_keepalive

--- extensions/node.sh
+++ extensions/node.sh
@@ -10,6 +10,13 @@ echo unique_string $unique_string
 echo data_center_name $data_center_name
 echo opscenter_location $opscenter_location
 
+echo "Going to set the TCP keepalive for now."
+sysctl -w net.ipv4.tcp_keepalive_time=120
+
+echo "Going to set the TCP keepalive permanently across reboots."
+echo "net.ipv4.tcp_keepalive_time = 120" >> /etc/sysctl.conf
+echo "" >> /etc/sysctl.conf
+
 opscenter_dns_name="opscenter$unique_string.$opscenter_location.cloudapp.azure.com"
 cluster_name="mycluster"
 # data_center_size
