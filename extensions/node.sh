486b5f7485b97879a786c9f4094739cf72e7de43
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-14 10:03:12 -0700

    add dbpasswd

--- extensions/node.sh
+++ extensions/node.sh
@@ -4,12 +4,15 @@ data_center_size=$1
 unique_string=$2
 data_center_name=$3
 opscenter_location=$4
+dbpasswd=$5
+#--dbpasswd , { "Ref" : "DBPassword"}
 
 echo "Input to node.sh is:"
 echo unique_string $unique_string
 echo data_center_name $data_center_name
 echo opscenter_location $opscenter_location
 
+# System setup/config
 # Copied in from general install scripts
 echo "Going to set the TCP keepalive for now."
 sysctl -w net.ipv4.tcp_keepalive_time=120
@@ -17,19 +20,23 @@ echo "Going to set the TCP keepalive permanently across reboots."
 echo "net.ipv4.tcp_keepalive_time = 120" >> /etc/sysctl.conf
 echo "" >> /etc/sysctl.conf
 
+# Mount data disk
+
 opscenter_dns_name="opscenter$unique_string.$opscenter_location.cloudapp.azure.com"
 cluster_name="mycluster"
-# data_center_size
-# data_center_name
 public_ip=`curl --retry 10 icanhazip.com`
 private_ip=`echo $(hostname -I)`
 node_id=$private_ip
+
+fault_domain=$(curl --max-time 50000 --retry 12 --retry-delay 50000 http://169.254.169.254/metadata/v1/InstanceInfo -s -S | sed -e 's/.*"FD":"\([^"]*\)".*/\1/')
+rack=FD$fault_domain
 
 echo "Calling addNode.py with the settings:"
 echo opscenter_dns_name $opscenter_dns_name
 echo cluster_name $cluster_name
 echo data_center_size $data_center_size
 echo data_center_name $data_center_name
+echo rack $rack
 echo public_ip $public_ip
 echo private_ip $private_ip
 echo node_id $node_id
@@ -38,7 +45,7 @@ apt-get update
 apt-get -y install unzip python-pip
 pip install requests
 
-cd /
+#cd /
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/master.zip
 unzip master.zip
 cd install-datastax-ubuntu-master/bin/lcm
@@ -48,6 +55,8 @@ cd install-datastax-ubuntu-master/bin/lcm
 --clustername $cluster_name \
 --dcsize $data_center_size \
 --dcname $data_center_name \
+--rack $rack \
 --pubip $public_ip \
 --privip $private_ip \
---nodeid $node_id
+--nodeid $node_id \
+--dbpasswd $dbpasswd
--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -2,5 +2,5 @@
 
 RESOURCE_GROUP=$1
 
-azure group create $1 "westus2"
+azure group create $1 "eastus"
 azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
