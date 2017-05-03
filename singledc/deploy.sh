da4e395c0e7a29f00fd731e950c71756d35c3e78
mtbbiker <eugenet@netstar.altech.co.za>
2017-05-03 16:33:16 +0200

    message

--- singledc/deploy.sh
+++ singledc/deploy.sh
@@ -6,12 +6,12 @@ RESOURCE_GROUP=$1
 if [ -z "$(which az)" ]
 then
     echo "CLI v2 'az' command not found, falling back to v1 'azure'"
-    azure group create $1 "eastus"
+    azure group create $1 "westeurope"
     azure group deployment create -f mainTemplate.json -e mainTemplateParameters.json $RESOURCE_GROUP dse
 
 else
     echo "CLI v2 'az' command found"
-    az group create --name $RESOURCE_GROUP --location "eastus"
+    az group create --name $RESOURCE_GROUP --location "westeurope"
     az group deployment create \
      --resource-group $RESOURCE_GROUP \
      --template-file mainTemplate.json \
