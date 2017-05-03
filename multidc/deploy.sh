fb21f22cf4808047e08202e29420edd962570a3d
mtbbiker <eugenet@netstar.altech.co.za>
2017-05-03 16:38:15 +0200

    Custom changes, use west europe

--- multidc/deploy.sh
+++ multidc/deploy.sh
@@ -8,12 +8,12 @@ python main.py
 if [ -z "$(which az)" ]
 then
     echo "CLI v2 'az' command not found, falling back to v1 'azure'"
-    azure group create $1 "eastus"
+    azure group create $1 "westeurope"
     azure group deployment create -f ./generatedTemplate.json $RESOURCE_GROUP dse
 
 else
     echo "CLI v2 'az' command found"
-    az group create --name $RESOURCE_GROUP --location "eastus"
+    az group create --name $RESOURCE_GROUP --location "westeurope"
     az group deployment create \
      --resource-group $RESOURCE_GROUP \
      --template-file generatedTemplate.json \
